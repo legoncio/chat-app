e574f6e2241a561d4be8bfe7109f618cf619ad0f
Leon Gonzalez <leonesteban@gmail.com>
2021-02-08 11:05:15 -0500

    fixed autoscroll

--- public/js/chat.js
+++ public/js/chat.js
@@ -23,22 +23,23 @@ const autoScroll = () => {
     const $newMessage = $messages.lastElementChild
 
     //Height if the new message
-    const newMessageStyles = getComputedStyle($newMessage)
-    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
-    const newMessageHeight = $newMessage.offsetHeight = newMessageMargin
+    //const newMessageStyles = getComputedStyle($newMessage)
+    //const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    //const newMessageHeight = $newMessage.offsetHeight = newMessageMargin
 
     //Visible height
-    const visibleHeight = $messages.offsetHeight
+    //const visibleHeight = $messages.offsetHeight
 
     //Height of messages container
-    const containerHeight = $messages.scrollHeight
+    //const containerHeight = $messages.scrollHeight
 
     //How far have I scrolled
-    const scrollOffset = $messages.scrollTop + visibleHeight
+    //const scrollOffset = $messages.scrollTop + visibleHeight
 
-    if(containerHeight - newMessageHeight <= scrollOffset){
-        $messages.scrollTop = $messages.scrollHeight
-    }
+    //if(containerHeight - newMessageHeight <= scrollOffset){
+        //$messages.scrollTop = $messages.scrollHeight
+        $newMessage.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
+    //}
 }
 socket.on('message', (message) => {
     console.log(message)
